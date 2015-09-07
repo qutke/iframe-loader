83f7c9950e1f44792d1b3894d6cdc09f113d8a79
Eric Wang <me@emptystack.net>
2015-09-07 21:27:09 +0800

    update

---
+++ index.js
@@ -0,0 +1,152 @@
+let childFrame, parentScreen
+let propsConfig = {}
+let config = {}
+
+const addListener = (eve, callback) => {
+  if (window.addEventListener) {
+    window.addEventListener(eve, callback, false)
+  } else {
+    window.attachEvent(`on${eve}`, callback)
+  }
+}
+
+const isString = (str) => {
+  return Object.prototype.toString.call(str) === '[object String]'
+}
+
+const addStyle = (obj, style) => {
+  for (let key in style) {
+    obj.style[key] = style[key]
+  }
+}
+
+const createIframe = (config, callback) => {
+  if (childFrame) {
+    return
+  }
+  childFrame = document.createElement('iframe')
+  childFrame.src = config.src
+  if (config.class) {
+    childFrame.class = config.class
+  }
+  addStyle(childFrame, config.style)
+  if (isString(config.renderTo)) {
+    config.renderTo = document.getElementById(config.renderTo)
+  }
+  config.renderTo.appendChild(childFrame)
+  // @todo 这里跨域的时候应该有问题
+  if (childFrame.attachEvent) {
+    childFrame.attachEvent('onload', callback)
+  } else {
+    childFrame.onload = callback
+  }
+}
+
+function parentMessage () {
+  let timer
+  const setHeight = (height) => {
+    window.top.postMessage({
+      code: 2,
+      height: height
+    }, '*')
+  }
+  const toFull = () => {
+    window.top.postMessage({
+      code: 3
+    }, '*')
+  }
+  const unFull = () => {
+    window.top.postMessage({
+      code: 4
+    }, '*')
+  }
+  this.full = () => {
+    if (timer) {
+      clearInterval(timer)
+    }
+    toFull()
+  }
+  this.auto = (full) => {
+    if (timer) {
+      clearInterval(timer)
+    }
+    let oldHeight = document.body.clientHeight
+    timer = setInterval(() => {
+      let newHeight = document.body.clientHeight
+      if (newHeight !== oldHeight || full) {
+        if (full) {
+          unFull()
+        }
+        full = false
+        oldHeight = document.body.clientHeight
+        setHeight(oldHeight + 20)
+      }
+    }, 100)
+  }
+}
+
+let [... [script]] = document.scripts
+const oldTypes = {1: 'hello', 2: 'setHeight', 3: 'toFull', 4: 'unFull'}
+const scriptConfig = script.attributes['config']
+const defaultConfig = {
+  src: 'https://realtime.qutke.com/info/559cc1f3310a3c46405c6f1d', // iframe地址
+  style: {width: '800px', height: '600px', border: 0, padding: 0, margin: 0}, // iframe样式
+  action: {
+    toFull: (data) => {
+      addStyle(childFrame, {
+        width: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        'z-index': 100000,
+        'min-height': `${document.documentElement.clientHeight - 5}px`
+      })
+    },
+    unFull: (data) => {
+      addStyle(childFrame, {
+        width: '100%',
+        position: 'relative',
+        top: '',
+        left: '',
+        'z-index': '',
+        'min-height': ''
+      })
+    },
+    setHeight: (data) => {
+      addStyle(childFrame, {height: data.height + 'px'})
+    },
+    hello: (data) => {
+      console.log('connected!')
+    }
+  }, // iframe监听的事件
+  renderTo: script.parentElement, // iframe父元素
+  id: 'QutkeIframe', // iframe的id
+  type: 'parent'
+}
+if (scriptConfig) {
+  try {
+    propsConfig = JSON.parse(scriptConfig.value)
+  } catch (e) {
+    console.log('config iframe error.')
+  }
+}
+
+config = Object.assign({}, defaultConfig, propsConfig)
+
+if (config.type === 'parent') {
+  createIframe(config, () => {
+    childFrame.contentWindow.postMessage('hello', '*') // 兼容马鹏的项目代码<_<
+    addListener('message', (e) => {
+      let code = e.data.code
+      let action = config.action[oldTypes[code]]
+      if (action) {
+        action(e.data)
+      }
+    })
+  })
+} else {
+  parentScreen = new parentMessage()
+  parentScreen.auto()
+}
+
+module.exports = parentScreen
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var DEBUG = process.env.NODE_ENV !== 'release'
+
+var devtool, entry, plugins, output
+
+if (DEBUG) {
+  devtool = 'source-map'
+  entry = [
+    // 在页面中自动增加http://localhost:3000/webpack-dev-server.js，可以检测目前webpack的运行情况
+    'webpack-dev-server/client?http://localhost:3000',
+    // 其做用是当源码发生修改以后可以live reloads
+    'webpack/hot/only-dev-server',
+    './index.js'
+  ]
+  plugins = [new webpack.HotModuleReplacementPlugin()]
+  output = {
+    path: path.join(__dirname, 'dist/build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  }
+} else {
+  devtool = false
+  entry = ['./index.js']
+  plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+      async: true
+
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+  output = {
+    path: path.join(__dirname, 'dist/build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  }
+}
+
+module.exports = {
+  // http://webpack.github.io/docs/configuration.html#devtool
+  devtool: devtool,
+  entry: entry,
+  output: output,
+  stats: {
+    colors: true,
+    reason: false
+  },
+  /*
+   *  Plugins
+   *  https://github.com/webpack/docs/wiki/list-of-plugins
+   *  HotModuleReplacementPlugin: 用于支持模块热更新的插件
+   *  NoErrorsPlugin: 保证程序不会在编译产生错误的时候终止
+   */
+  plugins: plugins,
+  /*
+   *  Resolve
+   *  http://webpack.github.io/docs/configuration.html#resolve
+   *  提供了包括 alias | root | modulesDirectories | fallback | extensions | packageMains | packageAlias | unsafeCache等的配置项
+   */
+  resolve: {
+    // alias: {
+    //   'react': path.join(__dirname, 'node_modules', 'react')
+    // },
+    extensions: ['', '.js', '.jsx']
+  },
+  // 简单来说，指定loader的解析模式和目录。
+  resolveLoader: {
+    'fallback': path.join(__dirname, 'node_modules')
+  },
+  module: {
+    loaders: require('./loaders.config')
+  }
+}
